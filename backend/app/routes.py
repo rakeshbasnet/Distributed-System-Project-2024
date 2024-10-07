380ecd810193da0c42555f9c67b56d5ab4cce15c
Rakesh Basnet <rakeshbasnet086@gmail.com>
2024-10-07 16:12:24 -0400

    Backend flask application with db connection for api to connect with frontend finished

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,22 @@
 from app import app
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template, jsonify
+from app.db import get, create
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return "Hello World!"
+    return render_template("index.html")
+
+
+@app.route('/api/users', methods=['GET'])
+def get_songs():
+    return get()
+
+
+@app.route('/api/add-user', methods=['POST'])
+def add_song():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    create(request.get_json())
+    return 'User Added'
